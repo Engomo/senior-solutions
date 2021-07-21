0530e941edab1fef08fed1932934fd564d41c6f8
Engomo <laszlo.vadkert@gmail.com>
2021-07-21 13:56:03 +0200

    Elsődleges kulcs

--- activitytracker-solution/src/main/java/activitytracker/Activity.java
+++ activitytracker-solution/src/main/java/activitytracker/Activity.java
@@ -8,7 +8,8 @@ import java.time.LocalDateTime;
 public class Activity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "act_id_gen")
+    @TableGenerator(name = "act_id_gen", table = "act_id_gen", pkColumnName = "id_gen", valueColumnName = "id_val")
     private Long id;
 
     @Column(name = "Start_Time", nullable = false)
