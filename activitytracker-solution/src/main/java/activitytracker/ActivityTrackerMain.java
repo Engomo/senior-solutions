f53bdca6c874b88860b7917cd2bfc11f59478cd3
Engomo <laszlo.vadkert@gmail.com>
2021-07-21 11:32:12 +0200

    További egyszerű műveletek JPA-val - gyakorlat

--- activitytracker-solution/src/main/java/activitytracker/ActivityTrackerMain.java
+++ activitytracker-solution/src/main/java/activitytracker/ActivityTrackerMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ActivityTrackerMain {
 
@@ -12,10 +13,36 @@ public class ActivityTrackerMain {
 
         EntityManager em = factory.createEntityManager();
 
-        insert(factory, em);
+        insert(em);
+
+        Activity activity = em.find(Activity.class, 2L);
+        System.out.println(activity);
+
+        em.getTransaction().begin();
+        activity = em.find(Activity.class, 2L);
+        activity.setDescription("Futás a tó körül");
+        em.getTransaction().commit();
+
+        System.out.println(activity);
+
+        List<Activity> activities = em.createQuery("select a from Activity a", Activity.class)
+                .getResultList();
+        System.out.println(activities);
+
+        em.getTransaction().begin();
+        activity = em.find(Activity.class, 2L);
+        em.remove(activity);
+        em.getTransaction().commit();
+
+        List<Activity> activitiesUpdated = em.createQuery("select a from Activity a", Activity.class)
+                .getResultList();
+        System.out.println(activitiesUpdated);
+
+        em.close();
+        factory.close();
     }
 
-    private static void insert(EntityManagerFactory factory, EntityManager em) {
+    private static void insert(EntityManager em) {
         em.getTransaction().begin();
 
         em.persist(new Activity(LocalDateTime.of(2021, 01, 21,11,02,35), "Biciklizés az Alföldön", ActivityType.BIKING));
@@ -23,7 +50,7 @@ public class ActivityTrackerMain {
         em.persist(new Activity(LocalDateTime.of(2021, 07, 21,11,02,35), "Kosárlabdázás a pályán", ActivityType.BASKETBALL));
 
         em.getTransaction().commit();
-        em.close();
-        factory.close();
+
+
     }
 }
